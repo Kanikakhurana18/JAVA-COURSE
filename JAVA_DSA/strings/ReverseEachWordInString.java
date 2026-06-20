4aacdb71f888e929b8315b3e3b5e695b622cc9a9
kanika Khurana <kanikakhurana595@gmail.com>
2026-06-20 16:28:25 +0530

    complete strings

--- JAVA_DSA/strings/ReverseEachWordInString.java
+++ JAVA_DSA/strings/ReverseEachWordInString.java
@@ -4,12 +4,80 @@ import java.util.Arrays;
 
 public class ReverseEachWordInString {
   public static void main(String[] args) {
-    String s = "   i like this program very much    ";
-    //trim: s = " i like this program very much "
-    System.out.println(s.trim());
-    String[] words = s.trim().split("\\s+");
-    //String ko words me tod deta ha Yahan multiple spaces ko separator maana gaya.
-        // This will now print the words nicely as an array: [i, like, this, program, very, much]
-        System.out.println("Split words: " + Arrays.toString(words));
+    // String s = "   i like this program very much    ";
+    // //trim: s = " i like this program very much "
+    // System.out.println(s.trim()); //O(1) extra space ek naya array bana raha hai.
+    // String[] words = s.trim().split("\\s+");
+    // //String ko words me tod deta ha Yahan multiple spaces ko separator maana gaya.
+    //     // This will now print the words nicely as an array: [i, like, this, program, very, much]
+    //     System.out.println("Split words: " + Arrays.deepToString(words));//array ke har element ko string bana ke return karta hai.
+    //     String[] arr = s.split(" ");
+        //"  hello   world  " op ["", "", "hello", "", "", "world"] Empty strings bhi aa jaati hain
+
+       //Built in words TC= O(n) SC =O(n)
+       String s= "   You have to do it     ";
+      String[] arr= s.trim().split("\\s+");
+      // Pehle bahar ke spaces hatao, phir words me tod do. To avoid empty string
+        StringBuilder ans = new StringBuilder(); //final answer ko store karega.
+        for(String word: arr){
+            ans.append(new StringBuilder(word).reverse()); //ek temporary StringBuilder word object banata usme reverse hota hai  hai.
+            // StringBuilder temp = new StringBuilder(word); //Har iteration mein naya banta hai:
+            // temp.reverse();
+            // ans.append(temp);
+            ans.append(" ");
+        } 
+        ans.deleteCharAt(ans.length()-1);
+        System.out.println(ans.toString());
+    
+
+        
+        
+        
+     //Two Pointer + StringBuilder tc O(n) and sc= O(n)
+        //  StringBuilder ans = new StringBuilder();
+        //  int i = 0;
+        //  int n = s.length();
+        //  while(i < n){ // spaces skip
+        //     while(i < n && s.charAt(i) == ' '){
+        //         i++;
+        //     }
+        //     if(i >= n) break;
+        //     int start = i;
+        //     while(i < n && s.charAt(i) != ' '){     // word find
+        //         i++;
+        //     }
+        //     int end = i - 1;
+        //     for(int j = end; j >= start; j--){ // reverse word
+        //         ans.append(s.charAt(j));
+        //     }
+        //     ans.append(" ");
+        // }
+        // if(ans.length() > 0){
+        //     ans.deleteCharAt(ans.length()-1);
+        // }
+        // return ans.toString();
+        
+        
+        
+    //   // TC= o(N^2) Kyuki + se har baar nayi String banti hai.
+    //   String[] words= s.trim().split("\\s+"); //split string into multiple words by space
+    //   String reverseString=""; //empty string
+    //   for(int j=0; j<words.length; j++){// for(String w: words){
+    //       String reverseWord="";
+    //       for(int i=words[j].length()-1;i>=0;i--){ //current word ko krna gai
+    //           reverseWord+= words[j].charAt(i); //add in backword direction
+    //       }
+    //     // for(int i=w.length()-1;i>=0;i--){
+    //     //       reverseWord+= w.charAt(i);
+    //       reverseString+= reverseWord;
+    //       if(j != words.length-1){
+    //         reverseString += " ";
+    //   }}
+    //   return reverseString;
+    // return reverseString.trim();
+
+
+
+
   }
 }
---
+++ JAVA_DSA/strings/ReverseWordInStringLeetcode.java
@@ -0,0 +1,68 @@
+package strings;
+
+public class ReverseWordInStringLeetcode {
+  public static void main(String[] args) {
+    String s = "Hello World   ";
+    // Tc = O(n) and SC= o(n) beacsue you are using string builder
+        StringBuilder ans = new StringBuilder();
+        int i= s.length()-1; //end position
+        while(i>=0){
+            while(i>=0 && s.charAt(i)==' '){ //trailing extra sapces remove krne ke liye use hota hai 
+                i--;
+            }
+            if(i<0) break;
+            int end =i;
+            while(i>=0 && s.charAt(i)!=' '){  // poora word find karo
+                i--;
+            }
+            ans.append(s.substring(i+1, end+1));
+            ans.append(" ");
+        }
+        ans.deleteCharAt(ans.length()-1);
+        System.out.println(ans.toString());
+
+
+        //Inplace reverse with space complextity O(n) tc= o(n)
+    //    String[] word= s.trim().split("\\s+"); //extra spaces remove //String[] words = {"the", "sky", "is", "blue"};
+    //    String clean = String.join(" ", word);//"the sky is blue" space se jod do
+    //    char[] arr= clean.toCharArray(); //so to convert into characters for reverse
+    //    int n= arr.length;
+    //    reverse(arr, 0 ,n-1); // Step 2: poori string reverse
+    //    int start =0; //har word reverse
+    //    for(int end=0;end<=n;end++){
+    //     if(end==n || arr[end]==' '){
+    //         reverse(arr, start, end-1);
+    //         start =end+1;
+    //     }
+    //    }
+    //  return new String(arr); // return String.valueOf(arr); //arr to string
+    // }
+    //  //helper function
+    //    public static void reverse(char[] arr, int left , int right){
+    //         while(left<right){
+    //             char temp = arr[left];
+    //             arr[left]= arr[right];
+    //             arr[right]= temp;
+    //             left++;
+    //             right--;
+    //         }
+
+    
+
+        //Using Built in methos but takes extra space O(N) TC= O(N)
+        // String[] arr = s.trim().split("\\s+");
+
+        // StringBuilder ans = new StringBuilder();
+
+        // for(int i = arr.length - 1; i >= 0; i--){
+
+        //     ans.append(arr[i]);
+
+        //     if(i != 0){
+        //         ans.append(" ");
+        //     }
+        // }
+
+        // return (ans.toString());
+  }
+}
